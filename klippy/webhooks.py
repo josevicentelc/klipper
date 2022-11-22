3206d5b6f4d85a045facedb16e8f9b034ad74cc5
Jose Vicente Lozano Copa <jv_lc@hotmail.com>
2022-11-22 13:36:12 +0100

    roolback

--- klippy/webhooks.py
+++ klippy/webhooks.py
@@ -332,8 +332,6 @@ class GCodeHelper:
         wh.register_endpoint("gcode/help", self._handle_help)
         wh.register_endpoint("gcode/script", self._handle_script)
         wh.register_endpoint("gcode/restart", self._handle_restart)
-        wh.register_endpoint("gcode/crash_test",
-                             self._handle_crash_test)
         wh.register_endpoint("gcode/firmware_restart",
                              self._handle_firmware_restart)
         wh.register_endpoint("gcode/subscribe_output",
@@ -344,10 +342,6 @@ class GCodeHelper:
         self.gcode.run_script(web_request.get_str('script'))
     def _handle_restart(self, web_request):
         self.gcode.run_script('restart')
-    def _handle_crash_test(self, web_request):
-        vara = 5
-        varb = 0
-        varcrash = vara / varb
     def _handle_firmware_restart(self, web_request):
         self.gcode.run_script('firmware_restart')
     def _output_callback(self, msg):
